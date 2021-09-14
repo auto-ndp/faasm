32ee804478c79440927d994c2f09fde71452860d
Jakub Szewczyk <git@kubasz.xyz>
2021-09-14 11:59:19 +0100

    Test mlr logging

--- src/scheduler/Scheduler.cpp
+++ src/scheduler/Scheduler.cpp
@@ -41,18 +41,21 @@ static thread_local std::unordered_map<std::string,
 
 MessageLocalResult::MessageLocalResult()
 {
-    event_fd = eventfd(0, EFD_CLOEXEC);
+    event_fd = eventfd(0, 0);
+    SPDLOG_WARN("MLR + {}", event_fd);
 }
 
 MessageLocalResult::~MessageLocalResult()
 {
     if (event_fd >= 0) {
+        SPDLOG_WARN("MLR - {}", event_fd);
         close(event_fd);
     }
 }
 
 void MessageLocalResult::set_value(std::unique_ptr<faabric::Message>&& msg)
 {
+    SPDLOG_WARN("MLR * {}", event_fd);
     this->promise.set_value(std::move(msg));
     eventfd_write(this->event_fd, (eventfd_t)1);
 }
