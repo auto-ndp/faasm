a3d313001b429ebb08eed54eb95bb28f5b94ac8d
Jakub Szewczyk <git@kubasz.xyz>
2021-10-25 17:39:43 +0100

    Update env

--- bin/cluster_env.sh
+++ bin/cluster_env.sh
@@ -2,25 +2,13 @@
 THIS_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]:-${(%):-%x}}))
 PROJ_ROOT=${THIS_DIR}/..
 
-export FAASM_VERSION=$(cat ${PROJ_ROOT}/VERSION)
-export CPP_VERSION=$(cat ${PROJ_ROOT}/clients/cpp/VERSION)
-export PYTHON_VERSION=$(cat clients/python/VERSION)
+set -o allexport
+source ${PROJ_ROOT}/.env
+set +o allexport
 
 # Mount our local build into the local cluster
 export FAASM_BUILD_MOUNT=/build/faasm
 export FAASM_LOCAL_MOUNT=/usr/local/faasm
-
-if [[ -z "$FAASM_CLI_IMAGE" ]]; then
-    export FAASM_CLI_IMAGE=kubasz51/faasm-cli:${FAASM_VERSION}
-fi
-
-if [[ -z "$CPP_CLI_IMAGE" ]]; then
-    export CPP_CLI_IMAGE=kubasz51/faasm-cpp-sysroot:${CPP_VERSION}
-fi
-
-if [[ -z "$PYTHON_CLI_IMAGE" ]]; then
-    export PYTHON_CLI_IMAGE=kubasz51/faasm-cpython:${PYTHON_VERSION}
-fi
 
 touch /tmp/faasm-monitor
 touch /tmp/faasm-monitor-storage
