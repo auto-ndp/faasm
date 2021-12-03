3c2c220f27de60fb639ad9936684e535ca1c63bf
Jakub Szewczyk <git@kubasz.xyz>
2021-12-03 18:31:07 +0000

    WIP: Uffd memory arena manager

--- include/wasm/UffdMemoryArenaManager.h
+++ include/wasm/UffdMemoryArenaManager.h
@@ -1,7 +1,103 @@
 #pragma once
 
+#include <atomic>
+#include <compare>
+#include <cstddef>
+#include <cstdint>
+
+#include <faabric/util/locks.h>
 #include <faabric/util/memory.h>
 
-class UffdMemoryArenaManager {
+#include <absl/container/btree_set.h>
 
+namespace uffd {
+
+struct UffdMemoryRange
+{
+    std::byte* mapStart = nullptr;
+    size_t mapBytes = 0;
+    std::atomic<size_t> validBytes = 0;
+
+    UffdMemoryRange() = delete;
+    ~UffdMemoryRange() noexcept;
+    explicit UffdMemoryRange(size_t pages, size_t alignmentLog2 = 0);
+    UffdMemoryRange(const UffdMemoryRange&) = delete;
+    UffdMemoryRange& operator=(const UffdMemoryRange&) = delete;
+    inline UffdMemoryRange(UffdMemoryRange&& rhs) noexcept
+    {
+        *this = std::move(rhs);
+    }
+    inline UffdMemoryRange& operator=(UffdMemoryRange&& rhs) noexcept
+    {
+        this->~UffdMemoryRange();
+        this->mapStart = rhs.mapStart;
+        this->mapBytes = rhs.mapBytes;
+        this->validBytes.store(rhs.validBytes.load(std::memory_order_acquire),
+                               std::memory_order_release);
+        rhs.mapStart = nullptr;
+        rhs.mapBytes = 0;
+        rhs.validBytes = 0;
+        return *this;
+    }
+
+    inline bool pointerInRange(const std::byte* ptr) const
+    {
+        size_t addr = (size_t)ptr;
+        return addr >= (size_t)mapStart && addr < (size_t)(mapStart + mapBytes);
+    }
+
+    inline bool pointerInValidRange(const std::byte* ptr) const
+    {
+        size_t addr = (size_t)ptr;
+        return addr >= (size_t)mapStart &&
+               addr < (size_t)(mapStart +
+                               validBytes.load(std::memory_order_acquire));
+    }
+
+    inline std::strong_ordering operator<=>(const UffdMemoryRange& rhs) const
+    {
+        return size_t(mapStart) <=> size_t(rhs.mapStart);
+    }
+
+    inline friend std::strong_ordering operator<=>(const UffdMemoryRange& umr,
+                                                   const std::byte* ptr)
+    {
+        if (umr.pointerInRange(ptr)) {
+            return std::strong_ordering::equal;
+        }
+        return size_t(umr.mapStart) <=> size_t(ptr);
+    }
+
+    inline friend std::strong_ordering operator<=>(const std::byte* ptr,
+                                                   const UffdMemoryRange& umr)
+    {
+        return umr <=> ptr;
+    }
 };
+
+class UffdMemoryArenaManager final
+{
+  public:
+    UffdMemoryArenaManager(const UffdMemoryArenaManager&) = delete;
+    UffdMemoryArenaManager& operator=(const UffdMemoryArenaManager&) = delete;
+    static UffdMemoryArenaManager& instance();
+
+    // Allocates a UFFD-backed memory range, and returns the pointer to the
+    // beginning of it.
+    std::byte* allocateRange(size_t pages, size_t alignmentLog2 = 0);
+
+    void freeRange(std::byte* start);
+
+  private:
+    std::shared_mutex mx;
+    faabric::util::UserfaultFd uffd;
+    using RangeSet = absl::btree_set<UffdMemoryRange, std::less<>>;
+    RangeSet ranges;
+
+    void resizeRangeImpl(RangeSet::iterator range, size_t newPages);
+
+    UffdMemoryArenaManager();
+    ~UffdMemoryArenaManager() = default;
+};
+
+}
--- src/wasm/UffdMemoryArenaManager.cpp
+++ src/wasm/UffdMemoryArenaManager.cpp
@@ -1,3 +1,125 @@
 #include "UffdMemoryArenaManager.h"
 
+#include <faabric/util/timing.h>
 
+#include <stdexcept>
+#include <sys/mman.h>
+
+namespace uffd {
+
+UffdMemoryRange::~UffdMemoryRange() noexcept
+{
+    if (this->mapStart != nullptr && this->mapBytes > 0) {
+        if (munmap(mapStart, mapBytes) < 0) {
+            perror("Warning: error unmapping UffdMemoryRange");
+        }
+        this->mapStart = nullptr;
+        this->mapBytes = 0;
+        this->validBytes = 0;
+    }
+}
+UffdMemoryRange::UffdMemoryRange(size_t pages, size_t alignmentLog2)
+{
+    const size_t numBytes = pages * 4096;
+    const size_t alignmentBytes = 1ULL << alignmentLog2;
+    void* unalignedBaseAddress = mmap(nullptr,
+                                      numBytes + alignmentBytes,
+                                      PROT_READ | PROT_WRITE,
+                                      MAP_PRIVATE | MAP_ANONYMOUS,
+                                      -1,
+                                      0);
+    if (unalignedBaseAddress == MAP_FAILED) {
+        throw std::system_error(
+          errno, std::generic_category(), "Can't mmap uffd memory range");
+    }
+
+    const size_t address = reinterpret_cast<size_t>(unalignedBaseAddress);
+    const size_t alignedAddress =
+      (address + alignmentBytes - 1) & ~(alignmentBytes - 1);
+    std::byte* alignedPtr = reinterpret_cast<std::byte*>(alignedAddress);
+
+    const size_t numHeadPaddingBytes = alignedAddress - address;
+    if (numHeadPaddingBytes > 0) {
+        if (munmap(unalignedBaseAddress, numHeadPaddingBytes) < 0) {
+            throw std::system_error(errno,
+                                    std::generic_category(),
+                                    "Can't munmap unaligned uffd memory range");
+        }
+    }
+
+    const size_t numTailPaddingBytes =
+      alignmentBytes - (alignedAddress - address);
+    if (numTailPaddingBytes > 0) {
+        if (munmap(alignedPtr + numBytes, numTailPaddingBytes) < 0) {
+            throw std::system_error(errno,
+                                    std::generic_category(),
+                                    "Can't munmap unaligned uffd memory range");
+        }
+    }
+
+    madvise(alignedPtr, numBytes, MADV_HUGEPAGE);
+    TracyAllocNS(alignedPtr, numBytes, 6, "UFFD");
+
+    this->mapStart = alignedPtr;
+    this->mapBytes = numBytes;
+    this->validBytes = 0;
+}
+
+UffdMemoryArenaManager& UffdMemoryArenaManager::instance()
+{
+    static UffdMemoryArenaManager umam;
+    return umam;
+}
+
+UffdMemoryArenaManager::UffdMemoryArenaManager()
+{
+    faabric::util::FullLock lock{ mx };
+    uffd.create(O_CLOEXEC, true);
+}
+
+std::byte* UffdMemoryArenaManager::allocateRange(size_t pages,
+                                                 size_t alignmentLog2)
+{
+    UffdMemoryRange range{ pages, alignmentLog2 };
+    std::byte* start = range.mapStart;
+    {
+        faabric::util::FullLock lock{ mx };
+        this->uffd.registerAddressRange(
+          size_t(range.mapStart), range.mapBytes, true, false);
+        this->ranges.insert(std::move(range));
+    }
+    return start;
+}
+
+void UffdMemoryArenaManager::freeRange(std::byte* start)
+{
+    faabric::util::FullLock lock{ mx };
+    auto range = this->ranges.find(start);
+    if (range == this->ranges.end()) {
+        throw std::runtime_error("Invalid pointer for UFFD range removal");
+    }
+    size_t mapStart = size_t(range->mapStart);
+    size_t mapLength = range->mapBytes;
+    uffd.unregisterAddressRange(mapStart, mapLength);
+    this->ranges.erase(range);
+}
+
+void UffdMemoryArenaManager::resizeRangeImpl(
+  UffdMemoryArenaManager::RangeSet::iterator range,
+  size_t newPages)
+{
+    const size_t newValidBytes = newPages * 4096;
+    const size_t oldValidBytes =
+      range->validBytes.load(std::memory_order_acquire);
+    if (newValidBytes == oldValidBytes) {
+        return;
+    }
+    range->validBytes.store(newValidBytes, std::memory_order_release);
+    if (newValidBytes < oldValidBytes) {
+        madvise((void*)(range->mapStart + newValidBytes),
+                oldValidBytes - newValidBytes,
+                MADV_DONTNEED);
+    }
+}
+
+}
