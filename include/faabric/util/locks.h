6eeda43b0f31caf6fdd20aa11f3f73ae4cc9e882
Jakub Szewczyk <git@kubasz.xyz>
2022-01-03 13:26:57 +0000

    Add tracy lock wrapper to utility types

--- include/faabric/util/locks.h
+++ include/faabric/util/locks.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <faabric/util/logging.h>
+#include <faabric/util/timing.h>
 
 #include <atomic>
 #include <condition_variable>
@@ -10,10 +11,19 @@
 
 #define DEFAULT_FLAG_WAIT_MS 10000
 
+#define TraceableMutex(varname) TracyLockable(std::mutex, varname)
+#define TraceableSharedMutex(varname)                                          \
+    TracySharedLockable(std::shared_mutex, varname)
+
 namespace faabric::util {
 typedef std::unique_lock<std::mutex> UniqueLock;
+typedef std::unique_lock<LockableBase(std::mutex)> UniqueTraceableLock;
 typedef std::unique_lock<std::shared_mutex> FullLock;
+typedef std::unique_lock<SharedLockableBase(std::shared_mutex)>
+  FullTraceableLock;
 typedef std::shared_lock<std::shared_mutex> SharedLock;
+typedef std::shared_lock<SharedLockableBase(std::shared_mutex)>
+  SharedTraceableLock;
 
 class FlagWaiter : public std::enable_shared_from_this<FlagWaiter>
 {
