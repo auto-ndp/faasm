a325887dbfbfe7706009b715ca57d2982f6a3c27
Raven Szewczyk <git@eigenraven.me>
2022-08-31 12:53:53 +0100

    Fix wrong conan cmake directory when embedded in faasm

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -11,7 +11,7 @@ list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
 if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
   message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
   file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
-                "${CMAKE_BINARY_DIR}/conan.cmake"
+                "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake"
                 TLS_VERIFY ON)
 endif()
 
