781de7b373214f6c4fbd3069a33a1e1a8f4c45da
Alan Nair <alannair1000@gmail.com>
2023-02-28 16:51:12 +0000

    Modified Scripts to build and sync OSD image 4
    Signed-off-by: Alan Nair <alannair1000@gmail.com>

--- bin/setup-osd.sh
+++ bin/setup-osd.sh
@@ -5,8 +5,8 @@ THIS_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]:-${(%):-%x}}))
 PROJ_ROOT=${THIS_DIR}/..
 NODE=WORKER
 
-# execute from leader node before initial deployment
-function setupleader {
+# execute from each node before initial deployment
+function setup {
   for id in 0 1 2 3 4
   do
     dd if=/dev/zero of=/ceph${id}.img bs=1 count=0 seek=100G
@@ -15,9 +15,20 @@ function setupleader {
     mkdir -p /mnt/ceph${id}
     mount -o user_xattr /dev/loop3${id} /mnt/ceph${id}
   done
+  rm dev/faasm-local/ceph-ceph-mon1/*
 }
 
-# execute from leader node AFTER initial deployment
+# setup worker
+# function setupworker {
+#   for id in 0 1 2 3 4
+#   do
+#     losetup /dev/loop3${id} /ceph${id}.img
+#     mkdir -p /mnt/ceph${id}
+#     mount -o user_xattr /dev/loop3${id} /mnt/ceph${id}
+#   done
+# }
+
+# execute from leader node AFTER initial deployment on each node 
 function sync {
   LEADERHOST=$(docker node ls --format "{{.Hostname}}" --filter node.label=rank=leader)
   for host in $(docker node ls --format "{{.Hostname}}")
@@ -27,19 +38,10 @@ function sync {
       scp ${PROJ_ROOT}/dev/faasm-local/ceph-ceph-mon1/* ${host}:${PROJ_ROOT}/dev/faasm-local/ceph-ceph-mon1/
       for id in 0 1 2 3 4
       do
-        scp /ceph${id}.img ${host}:/ceph${id}.img
+        # scp /ceph${id}.img ${host}:/ceph${id}.img
+        scp -r /mnt/ceph${id} ${host}:/mnt/ceph${id}
       done
     fi
-  done
-}
-
-# setup worker
-function setupworker {
-  for id in 0 1 2 3 4
-  do
-    losetup /dev/loop3${id} /ceph${id}.img
-    mkdir -p /mnt/ceph${id}
-    mount -o user_xattr /dev/loop3${id} /mnt/ceph${id}
   done
 }
 
@@ -59,10 +61,10 @@ then
   NODE=LEADER
 fi
 
-if [[ $1 == setup && ${NODE} == LEADER ]]
-then 
-  setupleader
-fi
+# if [[ $1 == setup && ${NODE} == LEADER ]]
+# then 
+#   setupleader
+# fi
 
 if [[ $1 == sync && ${NODE} == LEADER ]]
 then 
@@ -72,4 +74,9 @@ fi
 if [ $1 == clean ]
 then 
   clean
+fi
+
+if [ $1 == setup ]
+then 
+  setup
 fi
