b9d1f2cd09d54c708035740dba9568603c2463e4
Alan Nair <alannair1000@gmail.com>
2023-09-16 13:25:45 +0000

    Debug statements in run_osd

--- bin/run_ceph_osd.sh
+++ bin/run_ceph_osd.sh
@@ -22,6 +22,8 @@ while [[ ! -e "${MON_CEPH_DIR}/ceph.mon.keyring" ]]; do
     sleep 1
 done
 
+echo "Received ceph.mon.keyring" >> /osd/osd_log
+
 if [[ ! -e "/osd/osd_secret" ]]; then
     cp -a "${MON_CEPH_DIR}"/ceph.client.admin.keyring /etc/ceph/ceph.client.admin.keyring
     cp -a "${MON_CEPH_DIR}"/ceph.bootstrap-osd.keyring /var/lib/ceph/bootstrap-osd/ceph.keyring
@@ -29,32 +31,31 @@ if [[ ! -e "/osd/osd_secret" ]]; then
     OSD_UUID="$(uuidgen -n @dns -s -N $(hostname)-osd)"
     OSD_SECRET="$(ceph-authtool --gen-print-key)"
     echo OSD UUID ${OSD_UUID}
-    echo CEPH-OSD1 "$(uuidgen -n @dns -s -N ceph-osd1-osd)"
-    echo CEPH-OSD2 "$(uuidgen -n @dns -s -N ceph-osd2-osd)"
-    echo CEPH-OSD3 "$(uuidgen -n @dns -s -N ceph-osd3-osd)"
-    echo CEPH-OSD4 "$(uuidgen -n @dns -s -N ceph-osd4-osd)"
-    echo CEPH-OSD5 "$(uuidgen -n @dns -s -N ceph-osd5-osd)"
+
     OSD_ID=$(echo "{\"cephx_secret\": \"$OSD_SECRET\"}" | ceph osd new $OSD_UUID -i - -n client.admin -k /etc/ceph/ceph.client.admin.keyring)
 
     echo "$OSD_UUID" > /osd/osd_uuid
     echo "$OSD_SECRET" > /osd/osd_secret
     echo "$OSD_ID" > /osd/osd_id
 
-    # mkdir -p /var/lib/ceph/osd/ceph-$OSD_ID
-    # dd if=/dev/zero of=/ceph-$OSD_ID.img bs=1 count=0 seek=100G
-    # mkfs.ext4 /ceph-$OSD_ID.img
-    # mount -o user_xattr /ceph-$OSD_ID.img /var/lib/ceph/osd/ceph-$OSD_ID
-    # mount /dev/loop2${OSD_ID} /var/lib/ceph/osd/ceph-$OSD_ID
+    echo "Initialized ${OSD_UUID} ${OSD_SECRET} ${OSD_ID}" >> /osd/osd_log
 
     ceph-authtool --create-keyring /var/lib/ceph/osd/ceph-$OSD_ID/keyring --name osd.$OSD_ID --add-key $OSD_SECRET
+    echo "Created keyring /var/lib/ceph/osd/ceph-$OSD_ID/keyring" >> /osd/osd_log
+
     ceph-osd -i $OSD_ID --mkfs --osd-uuid $OSD_UUID
+    echo "ceph-osd init" >> /osd/osd_log
+
     chown -R ceph:ceph /var/lib/ceph/osd/ceph-$OSD_ID
 else
     OSD_UUID="$(cat /osd/osd_uuid)"
     OSD_SECRET="$(cat /osd/osd_secret)"
     OSD_ID="$(cat /osd/osd_id)"
+
+    echo "Old vals ${OSD_UUID} ${OSD_SECRET} ${OSD_ID}" >> /osd/osd_log
 fi
 
-# # Run ceph
+# Run ceph
 export TCMALLOC_MAX_TOTAL_THREAD_CACHE_BYTES=134217728
+echo "exec ceph-osd ${OSD_ID}" >> /osd/osd_log
 exec ceph-osd -f --cluster "ceph" --id "${OSD_ID}" --setuser ceph --setgroup ceph
