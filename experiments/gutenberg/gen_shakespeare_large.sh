92d6544a343b52a8e9f0d96ee435ddfef8788d8f
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-28 19:36:40 +0000

    revert commit

---
+++ experiments/gutenberg/gen_shakespeare_large.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the file paths
+source_file="complete_works_shakespeare.txt"
+destination_file="shakespeare_large.txt"
+
+# Define the number of times to append
+n=22
+
+if [ ! -e "$destination_file" ]; then
+    touch "$destination_file"
+fi
+
+
+# Loop n times and append the contents of source file to destination file
+for ((i=0; i<$n; i++))
+do
+    cat "$source_file" >> "$destination_file"
+done
+
+echo "Contents of $source_file appended to $destination_file $n times."
