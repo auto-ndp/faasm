057d132c5020404464e768a94e8688327c728db5
Donald Jennings <D.D.Jennings@outlook.com>
2024-03-03 14:41:54 +0000

    Add benchmarking for multiple objects invocation

--- faasmcli/faasmcli/tasks/benchmark.py
+++ faasmcli/faasmcli/tasks/benchmark.py
@@ -1,6 +1,6 @@
 from invoke import task
 
-from faasmcli.util.benchmarking import batch_async_aiohttp, sliding_window_impl
+from faasmcli.util.benchmarking import batch_async_aiohttp, sliding_window_impl, run_benchmark_multiple_objs
 
 
 @task
@@ -100,3 +100,53 @@ def latency_test(
         msg["forbid_ndp"] = forbid_ndp
     print("Payload:", msg)
     return sliding_window_impl(msg, {"Content-Type": "application/json"}, policy, iters, forbid_ndp)
+
+
+@task
+def throughput_test_multiple_objects(
+    ctx,
+    user,
+    func,
+    iters=10,
+    forbid_ndp=False,
+    policy="round_robin",
+    inputs=None,
+    py=False,
+    asynch=False,
+    poll=False,
+    cmdline=None,
+    mpi_world_size=None,
+    debug=False,
+    sgx=False,
+    graph=False,
+):
+    
+    if poll:
+        asynch = True
+        
+    msg = {
+        "user": user,
+        "function": func,
+        "async": asynch,
+    }
+
+    if sgx:
+        msg["sgx"] = sgx
+
+    if input:
+        msg["input_data"] = input.split(",")
+
+    if cmdline:
+        msg["cmdline"] = cmdline
+
+    if mpi_world_size:
+        msg["mpi_world_size"] = int(mpi_world_size)
+
+    if graph:
+        msg["record_exec_graph"] = graph
+        
+    if forbid_ndp:
+        print("Forbid NDP: ", forbid_ndp)
+        msg["forbid_ndp"] = forbid_ndp
+    print("Payload:", msg)
+    return run_benchmark_multiple_objs(msg, {"Content-Type": "application/json"}, iters, policy, forbid_ndp)
--- faasmcli/faasmcli/util/benchmarking.py
+++ faasmcli/faasmcli/util/benchmarking.py
@@ -16,6 +16,7 @@ from queue import Queue
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from collections import deque
+import itertools
 
 def batch_async_aiohttp(msg, headers, selected_balancer, n, forbid_ndp):
     ITERATIONS = int(n)
@@ -65,19 +66,7 @@ async def dispatch_func_async(session, url, data, headers):
     async with session.post(url, json=data, headers=headers) as response:
         end_time = time.perf_counter()
         await response.text()
-        return end_time - start_time
-
-def write_to_file(fp, results):
-    with open(fp, "a") as f:
-        
-        f.write("Batch size,Mean Latency,Median Latency,Time taken\n")
-        for dict in results:        
-            f.write(str(dict["batch_size"]) + ",")
-            f.write(str(dict["mean_latency"]) + ",")
-            f.write(str(dict["median_latency"]) + ",")
-            f.write(str(dict["time_taken"]) + "\n")
-        f.write("\n")
-        
+        return end_time - start_time        
         
 # =============================================================================
 # Sliding Window distribution
@@ -138,3 +127,74 @@ def sliding_window_impl(msg, headers, selected_balancer, n, forbid_ndp):
     print("Latencies: ", sum(latencies)/len(latencies))
     print("Median Latency: ", sorted(latencies)[len(latencies)//2])
     print("Responses received: ", len(latencies))
+    
+# =============================================================================
+# Multiple objects invocation
+# =============================================================================
+
+def run_benchmark_multiple_objs(data, headers, n, selected_balancer, forbid_ndp):
+    ITERATIONS = int(n)
+    results = []
+    for i in range(1, ITERATIONS):
+        latencies = []
+        start_time = time.perf_counter()
+        print("Running a batch of size: ", i)
+        print("Selected balancer: ", selected_balancer)
+        latencies = asyncio.run(batch_send_multiple_objs(data, headers, i, selected_balancer))
+        end_time = time.perf_counter()
+        print("Time taken to run batch: ", end_time - start_time)
+        result_dict = {
+            "batch_size": i,
+            "mean_latency" : sum(latencies)/len(latencies),
+            "median_latency": sorted(latencies)[len(latencies)//2],
+            "time_taken": end_time - start_time            
+        }
+        
+        print("Result: ", result_dict)
+        results.append(result_dict)
+    
+    # timestamp as str
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    
+    result_name = "{}_{}_{}_ndp_{}_iters_{}.csv".format(timestamp , data["function"], selected_balancer, not forbid_ndp, n)
+    write_to_file("./experiments/" + result_name, results)
+    print("Done running batches")
+    return results
+
+async def batch_send_multiple_objs(data, headers, n, selected_balancer):
+    # Create iterable of target objects
+    target_obj_iterator = itertools.cycle(data["input_data"])
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for _ in range(n):
+            with threading.Lock():
+                balancer = get_load_balance_strategy(selected_balancer)
+                worker_id = balancer.get_next_host(data["user"], data["function"])
+                data["input_data"] = next(target_obj_iterator)
+                print("Calling object: ", data["input_data"])
+                url = "http://{}:{}/f/".format(worker_id, 8080)
+                tasks.append(dispatch_func_async_multiple_objs(session, url, data, headers))
+                upload_load_balancer_state(balancer, selected_balancer, docker=True)
+        responses= await asyncio.gather(*tasks)
+    return responses
+
+async def dispatch_func_async_multiple_objs(session, url, data, headers):
+    start_time = time.perf_counter()    
+    async with session.post(url, json=data, headers=headers) as response:
+        end_time = time.perf_counter()
+        await response.text()
+        return end_time - start_time
+# =============================================================================
+# Utility functions
+# =============================================================================
+
+def write_to_file(fp, results):
+    with open(fp, "a") as f:
+        
+        f.write("Batch size,Mean Latency,Median Latency,Time taken\n")
+        for dict in results:        
+            f.write(str(dict["batch_size"]) + ",")
+            f.write(str(dict["mean_latency"]) + ",")
+            f.write(str(dict["median_latency"]) + ",")
+            f.write(str(dict["time_taken"]) + "\n")
+        f.write("\n")
